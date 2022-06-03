1df435fea7b993d0709c4eb9255d914884a3b7e5
LAPTOP-9D7K0V30\acer <amitkishore1234@gmail.com>
2022-06-03 19:49:54 +0530

    fixes

--- Scripts/Authentication/FirebaseMessaging.cs
+++ Scripts/Authentication/FirebaseMessaging.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Firebase;
 public class FirebaseMessaging : MonoBehaviour
 {
     public void Start()
     {
         Firebase.Messaging.FirebaseMessaging.TokenReceived += OnTokenReceived;
         Firebase.Messaging.FirebaseMessaging.MessageReceived += OnMessageReceived;
+        StartCrashalytics();
     }
 
     public void OnTokenReceived(object sender, Firebase.Messaging.TokenReceivedEventArgs token)
@@ -18,5 +19,29 @@ public class FirebaseMessaging : MonoBehaviour
     public void OnMessageReceived(object sender, Firebase.Messaging.MessageReceivedEventArgs e)
     {
         UnityEngine.Debug.Log("Received a new message from: " + e.Message.From);
+    }
+
+    void StartCrashalytics()
+    {
+        // Initialize Firebase
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                // Crashlytics will use the DefaultInstance, as well;
+                // this ensures that Crashlytics is initialized.
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here for indicating that your project is ready to use Firebase.
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
     }
 }
